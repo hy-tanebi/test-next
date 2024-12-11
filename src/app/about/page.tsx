564fd4acc6cf996be66e47833615edda12b5e2a6
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2024-12-12 05:42:23 +0900

    footerを変更

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: "about Pageeee",
+  description: "これはアバウトな感じ",
+};
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>これはaboutページ</p>
+      </main>
+      <footer>
+        <p>これはfooter</p>
+      </footer>
+    </div>
+  );
+}
